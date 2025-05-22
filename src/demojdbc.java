b704b8c7b84031e67186daf96e530f02ddc7f5a5
srajangupta144 <srajangupta144@gmail.com>
2025-05-22 19:18:03 +0530

    Creation of Statement after connection

--- src/demojdbc.java
+++ src/demojdbc.java
@@ -16,11 +16,18 @@ public class demojdbc {
         String url="jdbc:postgresql://localhost:5432/demo";
         String uname="postgres";
         String password="srajan";
+        String query="select sname from student where sid= 1";
 
         Class.forName("org.postgresql.Driver");    //optional
-        Connection con=DriverManager.getConnection(url, uname, password);
-
+        Connection con=DriverManager.getConnection(url, uname, password);  //Connection is made using Connection interface
         System.out.println("Connection completed");
+        Statement st= con.createStatement();            // Create Statement in the connection as it is an interface
+        ResultSet res= st.executeQuery(query);          //Statement return ResultSet
+        System.out.println(res.next());
+
+        con.close();                            // connection close
+        System.out.println("Connection Closed");
+
     }
 
 
