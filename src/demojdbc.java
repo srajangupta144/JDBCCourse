5f0682a537e698daf3cbf5448f084c8a63ff4b06
srajangupta144 <srajangupta144@gmail.com>
2025-06-09 23:22:10 +0530

    CRUD Operations

--- src/demojdbc.java
+++ src/demojdbc.java
@@ -16,14 +16,29 @@ public class demojdbc {
         String url="jdbc:postgresql://localhost:5432/demo";
         String uname="postgres";
         String password="srajan";
-        String query="select sname from student where sid= 1";
+        String query="delete from student where sid=4";           //chnge acc to operations needed
 
         Class.forName("org.postgresql.Driver");    //optional
         Connection con=DriverManager.getConnection(url, uname, password);  //Connection is made using Connection interface
         System.out.println("Connection completed");
         Statement st= con.createStatement();            // Create Statement in the connection as it is an interface
-        ResultSet res= st.executeQuery(query);          //Statement return ResultSet
-        System.out.println(res.next());
+
+        st.execute(query);
+        //FOR GET DATA QUERIES
+        /*       ResultSet res= st.executeQuery(query);          //Statement return ResultSet
+//        res.next();                                     //ByDefault the position of index for row of ResultSet if before the data so to make it from first
+//        String name=res.getString("sname");     // column name is given
+//        System.out.println("Name of Student is:"+name);
+
+        while(res.next()){
+            System.out.print(res.getInt(1)+" - ");
+            System.out.print(res.getString(2)+" - ");
+            System.out.println(res.getInt(3));
+        }
+
+  */
+
+
 
         con.close();                            // connection close
         System.out.println("Connection Closed");
